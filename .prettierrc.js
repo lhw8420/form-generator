1da1934e05528b2aedacd470e1ad6f6762444fdf
lhw8420 <1980370947@qq.com>
2021-06-10 23:02:09 +0800

    [style]: 格式化代码规范配置
    1、格式化代码配置

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'auto', // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
+  'prettier.tslintIntegration': false // 不让prettier使用tslint的代码格式进行校验
+};
